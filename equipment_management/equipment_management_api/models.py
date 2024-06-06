40b7cbdc9014b8d7e7f7b990a5d3aa2d6fb68341
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-06-06 10:36:46 +0900

    models.pyにcourseクラスを追加#3

--- equipment_management/equipment_management_api/models.py
+++ equipment_management/equipment_management_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField('学科名', max_length=100)
+    course_year = models.IntegerField('学科年')
+    delete_flag = models.BooleanField('削除フラグ', default=False)
+
+    class Meta:
+        db_table = 'courses'
+        verbose_name = '学科'
+        verbose_name_plural = '学科'
+
+    def __str__(self):
+        return self.name
