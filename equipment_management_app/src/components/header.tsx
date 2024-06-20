ceffed174337404d5183e2374e3f4e7eb8d188c9
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2024-06-20 10:39:25 +0900

    ハンバーガーメニュー試作作成

--- equipment_management_app/src/components/header.tsx
+++ equipment_management_app/src/components/header.tsx
@@ -10,49 +10,76 @@ import {
   LogoutIcon,
 } from "../images/icon";
 
-function header() {
+function Header() {
+  const hamburger_menu = () => {
+    const hamburger = document.getElementsByClassName("hamburger");
+    const hamburger_icons = document.getElementsByClassName("hamburger_icon");
+    const navs = document.getElementsByClassName("nav");
+    for (let i = 0; i < hamburger_icons.length; i++) {
+      hamburger_icons[i].classList.toggle("close");
+    }
+
+    for (let i = 0; i < navs.length; i++) {
+      hamburger_icons[i].classList.toggle("close");
+    }
+  };
+
   return (
     <div>
       <Toolbar className="toolbar">
         <h1 className="title">備品管理システム（仮）</h1>
         <div className="history">
-          {/* 貸出履歴管理icon */}
-          <HistoryIcon />
+          <HistoryIcon /> {/* 貸出履歴管理icon */}
           <p className="header_text">貸出履歴管理</p>
         </div>
         <div className="equipment_management">
-          {/* 備品管理icon */}
+          <EquipmentIcon /> {/* 備品管理icon */}
           <p className="header_text">備品管理</p>
         </div>
         <div className="student_management">
-          {/* 学生管理icon */}
-
+          <StudentIcon /> {/* 学生管理icon */}
           <p className="header_text">学生管理</p>
         </div>
         <div className="teacher_management">
-          {/* 教員管理のicon */}
-
+          <TeacherIcon /> {/* 教員管理のicon */}
           <p className="header_text">教員管理</p>
         </div>
         <div className="invetory_management">
-          {/*棚卸管理のicon*/}
-
+          <InvetoryIcon /> {/*棚卸管理のicon*/}
           <p className="header_text">棚卸管理</p>
         </div>
         <div className="category_management">
-          {/*カテゴリー管理のicon*/}
+          <CategoryIcon /> {/*棚卸管理のicon*/}
           <p className="header_text">カテゴリー管理</p>
         </div>
 
         <div className="logout">
-          {/* ログアウトのicon */}
+          <LogoutIcon /> {/* ログアウトのicon */}
           <p className="header_text">ログアウト</p>
         </div>
 
-        <div className="hamburger">{/*ハンバーガーのicon */}</div>
+        <div className="hamburger" onClick={hamburger_menu}>
+          {/*ハンバーガーのicon */}
+          <div className="hamburger_icon">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <nav className="nav">
+          <ul>
+            <li className="nav_item">貸出履歴管理</li>
+            <li className="nav_item">備品管理</li>
+            <li className="nav_item">学生管理</li>
+            <li className="nav_item">教員管理</li>
+            <li className="nav_item">棚卸管理</li>
+            <li className="nav_item">カテゴリー管理</li>
+            <li className="nav_item">ログアウト</li>
+          </ul>
+        </nav>
       </Toolbar>
     </div>
   );
 }
 
-export default header;
+export default Header;
--- equipment_management_app/src/images/icon.tsx
+++ equipment_management_app/src/images/icon.tsx
@@ -8,35 +8,35 @@ const HistoryIcon = () => (
 );
 
 const EquipmentIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon">
     <title>備品管理</title>
     <path d="M3,4H7V8H3V4M9,5V7H21V5H9M3,10H7V14H3V10M9,11V13H21V11H9M3,16H7V20H3V16M9,17V19H21V17H9" />
   </svg>
 );
 
 const StudentIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon">
     <title>学生管理</title>
     <path d="M16 8C16 10.21 14.21 12 12 12C9.79 12 8 10.21 8 8L8.11 7.06L5 5.5L12 2L19 5.5V10.5H18V6L15.89 7.06L16 8M12 14C16.42 14 20 15.79 20 18V20H4V18C4 15.79 7.58 14 12 14Z" />
   </svg>
 );
 
 const TeacherIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon">
     <title>教員管理</title>
     <path d="M20,17A2,2 0 0,0 22,15V4A2,2 0 0,0 20,2H9.46C9.81,2.61 10,3.3 10,4H20V15H11V17M15,7V9H9V22H7V16H5V22H3V14H1.5V9A2,2 0 0,1 3.5,7H15M8,4A2,2 0 0,1 6,6A2,2 0 0,1 4,4A2,2 0 0,1 6,2A2,2 0 0,1 8,4Z" />
   </svg>
 );
 
 const InvetoryIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon">
     <title>棚卸管理</title>
     <path d="M19,8V5H5V8H9A3,3 0 0,0 12,11A3,3 0 0,0 15,8H19M19,3A2,2 0 0,1 21,5V12A2,2 0 0,1 19,14H5A2,2 0 0,1 3,12V5A2,2 0 0,1 5,3H19M3,15H9A3,3 0 0,0 12,18A3,3 0 0,0 15,15H21V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V15Z" />
   </svg>
 );
 
 const CategoryIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon">
     <title>カテゴリー管理</title>
     <path d="M11,13.5V21.5H3V13.5H11M12,2L17.5,11H6.5L12,2M17.5,13C20,13 22,15 22,17.5C22,20 20,22 17.5,22C15,22 13,20 13,17.5C13,15 15,13 17.5,13Z" />
   </svg>
