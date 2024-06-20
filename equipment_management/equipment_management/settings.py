426da87afe5988f9b238d02f74371a31ccadc415
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-06-20 10:53:04 +0900

    inventoriesテーブルのマイグレーションの作成

--- equipment_management/equipment_management/settings.py
+++ equipment_management/equipment_management/settings.py
@@ -123,6 +123,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+DATE_FORMAT = 'Y/m/d'
+
+USE_L10N = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
