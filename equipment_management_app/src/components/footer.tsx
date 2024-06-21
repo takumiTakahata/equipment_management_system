4ea3ddf8f54f5e687fe308918611a6248a80523d
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-06-21 10:46:11 +0900

    BottomNavigationの追加

--- equipment_management_app/src/components/footer.tsx
+++ equipment_management_app/src/components/footer.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
@@ -11,3 +12,23 @@ import Borrowing from "@mdi/react";
 import { mdiPlaylistPlus } from "@mdi/js";
 import Logout from "@mdi/react";
 import { mdiLogout } from "@mdi/js";
+
+export default function Footer() {
+  const [value, setValue] = useState(0);
+  return (
+    <div>
+      <footer>
+        <Box className="bos">
+          <BottomNavigation
+            className="custom-bottom-navigation"
+            showLabels //ラベルが常に表示される
+            value={value} //現在選択されているナビゲーションアイテムの値を表す
+            onChange={(event, newValue) => {
+              setValue(newValue); //ナビが選択されたときに呼び出す関数を示す
+            }}
+          ></BottomNavigation>
+        </Box>
+      </footer>
+    </div>
+  );
+}
