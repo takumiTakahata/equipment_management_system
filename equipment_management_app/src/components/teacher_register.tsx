3b3a019e96100ddd3e71ea04f6b880ac2aa768c4
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2024-06-26 09:59:12 +0900

    名前のvalidation作成

--- equipment_management_app/src/components/teacher_register.tsx
+++ equipment_management_app/src/components/teacher_register.tsx
@@ -4,24 +4,42 @@ import { useForm } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
 import { Button } from "@mui/material";
 import Header from "./header";
+import { log } from "console";
+
+interface FormInputs {
+  name_error: string;
+  mail_error: string;
+}
 
 function TeacherRegister() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+
+  const onSubmit = (data: FormInputs) => {
+    console.log(data);
+  };
   return (
     <div>
       <Header />
       <h1>教員登録</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <p>名前</p>
-        <TextField />
+        <TextField
+          {...register("name_error", {
+            required: "名前を入力してください",
+          })}
+        />
+        <ErrorMessage errors={errors} name="name_error" as="p" />
         <p>メールアドレス</p>
         <TextField />
         <p>パスワード</p>
         <TextField />
         <p>パスワード（確認）</p>
         <TextField />
-        <Button variant="contained" href="#">
-          登録
-        </Button>
+        <Button type="submit">登録</Button>
       </form>
     </div>
   );
