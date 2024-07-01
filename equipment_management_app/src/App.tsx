376f35b23f923efddfcf6543f5c35a41bbda9b08
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2024-07-01 14:58:43 +0900

    いらないimport文削除

--- equipment_management_app/src/App.tsx
+++ equipment_management_app/src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/header"; // Headerコンポーネントをインポート
 import TeacherRegister from "./components/teacher_register";
 const App = () => {
   return (
