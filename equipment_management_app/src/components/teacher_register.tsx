a67d48ab4e3fdaaa0aaa6458b1351d05976dac36
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2024-07-07 10:31:16 +0900

    各項目を中心に配置・marginを適用

--- equipment_management_app/src/components/teacher_register.tsx
+++ equipment_management_app/src/components/teacher_register.tsx
@@ -53,113 +53,121 @@ function TeacherRegister() {
   return (
     <div id="teacher_register">
       <Header />
-      <h1 className="title">教員登録</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="column">
+      <div className="center">
+        <h1 className="page_title">教員登録</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="name_container">
             <p className="name_title">名前</p>
             <p className="required_txt">※必須</p>
           </div>
-          <TextField
-            {...register("name", {
-              required: "名前を入力してください",
-            })}
-          />
-          <ErrorMessage
-            errors={errors}
-            name="name"
-            as="p"
-            className="error_message"
-          />
+          <div className="name_input">
+            <TextField
+              {...register("name", {
+                required: "名前を入力してください",
+              })}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="name"
+              as="p"
+              className="error_message"
+            />
+          </div>
           <div className="eamil_container">
             <p className="mail_title">メールアドレス</p>
             <p className="required_txt">※必須</p>
           </div>
-          <TextField
-            {...register("mail", {
-              required: "メールアドレスを入力してください",
-              pattern: {
-                value: /^[a-zA-Z0-9.]+@morijyobi\.ac\.jp$/,
-                message: "盛ジョビのメールアドレスを入力してください",
-              },
-            })}
-          />
-          <ErrorMessage
-            errors={errors}
-            name="mail"
-            as="p"
-            className="error_message"
-          />
+          <div className="mail_input">
+            <TextField
+              {...register("mail", {
+                required: "メールアドレスを入力してください",
+                pattern: {
+                  value: /^[a-zA-Z0-9.]+@morijyobi\.ac\.jp$/,
+                  message: "盛ジョビのメールアドレスを入力してください",
+                },
+              })}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="mail"
+              as="p"
+              className="error_message"
+            />
+          </div>
 
           <div className="pass_container">
             <p className="pass_title">パスワード（8文字以上 半角英数字）</p>
             <p className="required_txt">※必須</p>
           </div>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            {...register("password", {
-              required: "パスワードを入力してください",
-              pattern: {
-                value: /^(?=.*[A-Z]|[a-z])(?=.*\d)[A-Za-z0-9]{8}$/, // 入力規則 8文字以上
-                message: "パスワードの形式が間違っています",
-              },
-            })}
-          />
-          <ErrorMessage
-            errors={errors}
-            name="password"
-            as="p"
-            className="error_message"
-          />
+          <div className="pass_input">
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              {...register("password", {
+                required: "パスワードを入力してください",
+                pattern: {
+                  value: /^(?=.*[A-Z]|[a-z])(?=.*\d)[A-Za-z0-9]{8}$/, // 入力規則 8文字以上
+                  message: "パスワードの形式が間違っています",
+                },
+              })}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="password"
+              as="p"
+              className="error_message"
+            />
+          </div>
 
           <div className="passch_container">
             <p className="passch_title">パスワード（確認）</p>
             <p className="required_txt">※必須</p>
           </div>
-          <OutlinedInput
-            id="outlined-adornment-password_c"
-            type={showPassword_C ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword_C}
-                  onMouseDown={handleMouseDownPassword_C}
-                  edge="end"
-                >
-                  {showPassword_C ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            {...register("check_password", {
-              required: "パスワード（確認用）を入力してください",
-              validate: (value) => {
-                return value === password || "パスワードが間違がっています";
-              },
-            })}
-          />
-          <ErrorMessage
-            errors={errors}
-            name="check_password"
-            as="p"
-            className="error_message"
-          />
-        </div>
-        <Button type="submit">登録</Button>
-      </form>
+          <div className="passch_input">
+            <OutlinedInput
+              id="outlined-adornment-password_c"
+              type={showPassword_C ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword_C}
+                    onMouseDown={handleMouseDownPassword_C}
+                    edge="end"
+                  >
+                    {showPassword_C ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              {...register("check_password", {
+                required: "パスワード（確認用）を入力してください",
+                validate: (value) => {
+                  return value === password || "パスワードが間違がっています";
+                },
+              })}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="check_password"
+              as="p"
+              className="error_message"
+            />
+          </div>
+          <Button type="submit">登録</Button>
+        </form>
+      </div>
     </div>
   );
 }
