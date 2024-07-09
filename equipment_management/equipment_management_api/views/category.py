b76b3953da5993fec7e7bbfb4f733303c6413818
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-07-09 12:10:14 +0900

    POST時の処理を追加

--- equipment_management/equipment_management_api/views/category.py
+++ equipment_management/equipment_management_api/views/category.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets
 from ..models import Category
 from ..serializers import category_serializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class CategoryViewSet(viewsets.ModelViewSet):
   queryset = Category.objects.all()
   serializer_class = category_serializer
 
+@api_view(['POST'])
+def category_register_view(request):
+  category_name = request.data.get('category_name')
+  return Response({'category_name': category_name})
