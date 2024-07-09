23d3a5be95126d51e924955eef53cd06b89a9136
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-07-09 12:43:42 +0900

    シリアライザー作成

---
+++ equipment_management/equipment_management_api/serializers/teacher_register.py
@@ -0,0 +1,9 @@
+# accounts/serializers.py
+from rest_framework import serializers
+from .models import User
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('name', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
