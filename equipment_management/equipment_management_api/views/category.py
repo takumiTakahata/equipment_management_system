90aee086807ce2e9cd144efc035ff4da21e6e3fd
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-07-10 10:54:27 +0900

    categoryの登録操作の修正

--- equipment_management/equipment_management_api/views/category.py
+++ equipment_management/equipment_management_api/views/category.py
@@ -1,14 +1,18 @@
-from rest_framework import viewsets
-from ..models import Category
-from ..serializers import category_serializer
+from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from ..serializers import CategorySerializer
 
-class CategoryViewSet(viewsets.ModelViewSet):
-  queryset = Category.objects.all()
-  serializer_class = category_serializer
-
+# POSTの時の登録処理
 @api_view(['POST'])
 def category_register_view(request):
-  category_name = request.data.get('category_name')
-  return Response({'category_name': category_name})
+    # request.dataをシリアライザーに渡す
+    serializer = CategorySerializer(data=request.data)
+    if serializer.is_valid():
+        # シリアライザーを使用してデータベースに保存
+        serializer.save()
+        return Response({'message': 'Category successfully registered', 'category_name': serializer.validated_data['name']}, status=status.HTTP_201_CREATED)
+    else:
+        # バリデーションエラーを処理
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
