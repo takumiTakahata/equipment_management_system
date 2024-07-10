5ec678e80d22d9417ee92f460165b1e44dad68d0
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-07-10 10:53:14 +0900

    categoryのfieldsの修正

--- equipment_management/equipment_management_api/serializers/category_serializer.py
+++ equipment_management/equipment_management_api/serializers/category_serializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['name']
+        fields = '__all__'
