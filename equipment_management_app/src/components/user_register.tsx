e474a52a9bbdf29215309128b38fd0d8c702ea2e
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-07-11 10:51:27 +0900

    subject,gradeの追加

--- equipment_management_app/src/components/user_register.tsx
+++ equipment_management_app/src/components/user_register.tsx
@@ -9,3 +9,37 @@ import Visibility from "@mui/icons-material";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import MenuItem from "@mui/material";
 import "./user_register.css";
+
+const subject = [
+  {
+    value: "情報システム科",
+    label: "情報システム科",
+  },
+  {
+    value: "総合システム科",
+    label: "総合システム科",
+  },
+  {
+    value: "高度情報工学科",
+    label: "高度情報工学科",
+  },
+];
+
+const grade = [
+  {
+    value: "1年",
+    label: "1年",
+  },
+  {
+    value: "2年",
+    label: "2年",
+  },
+  {
+    value: "3年",
+    label: "3年",
+  },
+  {
+    value: "4年",
+    label: "4年",
+  },
+];
