64e11e20183ecb259ed0fd3197645eee93adbb26
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-07-11 10:22:51 +0900

    db処理をviewsでやる

--- equipment_management/equipment_management_api/serializers/teacher_serializer.py
+++ equipment_management/equipment_management_api/serializers/teacher_serializer.py
@@ -6,13 +6,3 @@ class TeacherSerializer(serializers.ModelSerializer):
         model = User
         fields = ('name', 'email', 'password')
         
-    def create(self,validated_data):
-        user = User.objects.create_user(
-            name=validated_data['name'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        user.admin_flag = validated_data.get('admin_flag', False)
-        user.delete_flag = validated_data.get('delete_flag', False)
-        user.save()
-        return user
