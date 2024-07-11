d3bbe3e046b0f83612a5bf2491f8c2c4040c79d1
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-07-11 10:43:18 +0900

    post処理記述

--- equipment_management/equipment_management_api/views/teacher.py
+++ equipment_management/equipment_management_api/views/teacher.py
@@ -1,17 +1,19 @@
-from rest_framework import generics
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User
 from ..serializers.teacher_serializer import TeacherSerializer
 
-
-class TeacherRegistrationView(generics.CreateAPIView):
-  queryset = User.objects.all()
-  serializer_class = TeacherSerializer
-    
-  def post(self, request, *args, **kwargs):
-    serializer = self.get_serializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    self.perform_create(serializer)
-    headers = self.get_success_headers(serializer.data)
-    return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+# POSTの時の登録処理
+@api_view(['POST'])
+def teacher_register_view(request):
+  # request.dataをシリアライザーに渡す
+  serializer = TeacherSerializer(data=request.data)
+  if serializer.is_valid():
+    # シリアライザーを使用してデータベースに保存
+    serializer.save()
+    return Response({'message': 'Category successfully registered', 'category_name': serializer.validated_data['name']}, status=status.HTTP_201_CREATED)
+  else:
+    # バリデーションエラーを処理
+    print(serializer.errors)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
