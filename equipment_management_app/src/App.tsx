99ac9055215f4bfe8a3f6f926f2d3469c3f5a0ba
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-07-17 10:42:53 +0900

    routeの追加

--- equipment_management_app/src/App.tsx
+++ equipment_management_app/src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import TeacherRegister from "./components/teacher_register";
+import UserRoute from "./routes/user_route";
 const App = () => {
   return (
     <>
       <TeacherRegister />
+      <Router>
+        <UserRoute />
+      </Router>
     </>
   );
 };
