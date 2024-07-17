673ccf5159d0e5ae0ec4c56d9babecea8e8c4aa3
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-07-17 10:10:30 +0900

    ハッシュ化処理追加

--- equipment_management/equipment_management_api/views/teacher.py
+++ equipment_management/equipment_management_api/views/teacher.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.hashers import make_password
 from ..serializers.teacher_serializer import TeacherSerializer
 
 # POSTの時の登録処理
@@ -9,6 +10,10 @@ def teacher_register_view(request):
   # request.dataをシリアライザーに渡す
   serializer = TeacherSerializer(data=request.data)
   if serializer.is_valid():
+    # パスワードをハッシュ化
+    hashed_password = make_password(serializer.validated_data['password'])
+    # ハッシュ化されたパスワードを再設定
+    serializer.validated_data['password'] = hashed_password
     # シリアライザーを使用してデータベースに保存
     serializer.save()
     return Response({'message': 'Teacher successfully registered', 'username': serializer.validated_data['username']}, status=status.HTTP_201_CREATED)
