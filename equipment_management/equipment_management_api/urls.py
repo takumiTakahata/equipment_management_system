ce80575c84d3fb48de17916a65117bff8b3f615d
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-07-17 10:42:53 +0900

    categoryのpathの設定

--- equipment_management/equipment_management_api/urls.py
+++ equipment_management/equipment_management_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import login_view
-from .views import category_register_view
+from .views import CategoryView
 
 router = DefaultRouter()
 # router.register(r'users', views.UserViewSet)
@@ -9,5 +9,5 @@ router = DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', login_view),
-    path('category/', category_register_view),
+    path('category/', CategoryView.as_view()),
 ]
