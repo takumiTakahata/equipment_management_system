bf6386df0b99e4c937223500317952880e0b827b
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2024-07-18 10:22:48 +0900

    貸出承認ページ作成・routeの設定

--- equipment_management_app/src/routes/admin_route.tsx
+++ equipment_management_app/src/routes/admin_route.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import TeacherTop from "../components/teacher_top";
+import LoanApproval from "../components/loan_approval";
 
 function AdminRoute() {
   return (
@@ -7,6 +8,8 @@ function AdminRoute() {
       <Routes>
         {/* 教員TOP画面 */}
         <Route path="/teacher_top" element={<TeacherTop />} />
+        {/* 貸出承認画面 */}
+        <Route path="/loan_approval" element={<LoanApproval />} />
       </Routes>
     </div>
   );
