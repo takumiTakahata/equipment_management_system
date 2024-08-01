cc3d6497878f81bc01d19ad6c32be102622a7538
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-08-01 23:28:09 +0900

    書式を変更

--- equipment_management/equipment_management_api/views/teacher.py
+++ equipment_management/equipment_management_api/views/teacher.py
@@ -1,23 +1,23 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.hashers import make_password
 from ..serializers.teacher_serializer import TeacherSerializer
+from rest_framework.views import APIView
 
-# POSTの時の登録処理
-@api_view(['POST'])
-def teacher_register_view(request):
-  # request.dataをシリアライザーに渡す
-  serializer = TeacherSerializer(data=request.data)
-  if serializer.is_valid():
-    # パスワードをハッシュ化
-    hashed_password = make_password(serializer.validated_data['password'])
-    # ハッシュ化されたパスワードを再設定
-    serializer.validated_data['password'] = hashed_password
-    # シリアライザーを使用してデータベースに保存
-    serializer.save()
-    return Response({'message': 'Teacher successfully registered', 'username': serializer.validated_data['username']}, status=status.HTTP_201_CREATED)
-  else:
-    # バリデーションエラーを処理
-    print(serializer.errors)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class TeacherView(APIView):
+   
+  # POSTの時の登録処理
+  def post(self, request):
+    serializer = TeacherSerializer(data=request.data)
+    if serializer.is_valid():
+      # パスワードをハッシュ化
+      hashed_password = make_password(serializer.validated_data['password'])
+      # ハッシュ化されたパスワードを再設定
+      serializer.validated_data['password'] = hashed_password
+      # シリアライザーを使用してデータベースに保存
+      serializer.save()
+      return Response({'message': 'Teacher successfully registered', 'username': serializer.validated_data['username']}, status=status.HTTP_201_CREATED)
+    else:
+      # バリデーションエラーを処理
+      print(serializer.errors)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
