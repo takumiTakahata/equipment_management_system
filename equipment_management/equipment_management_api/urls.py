a9c8d209732194f7da5196f4f5360b0da041e570
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-08-01 23:28:49 +0900

    インポート名を変更

--- equipment_management/equipment_management_api/urls.py
+++ equipment_management/equipment_management_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import login_view
 from .views import CategoryView
-from .views import teacher_register_view
+from .views import TeacherView
 
 router = DefaultRouter()
 # router.register(r'users', views.UserViewSet)
@@ -11,5 +11,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('login/', login_view),
     path('category/', CategoryView.as_view()),
-    path('teacher/', teacher_register_view),
+    path('teacher/', TeacherView.as_view()),
 ]
