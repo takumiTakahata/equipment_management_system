0b1d52d8b76467df209714493b06d5aa2d3be385
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-08-11 16:01:05 +0900

    パスワード入力欄,エラーの表示,import文追記

--- app/src/components/user_register.tsx
+++ app/src/components/user_register.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
-import { TextField } from "@mui/material";
-import OutlinedInput from "@mui/material";
-import InputAdornment from "@mui/material";
-import IconButton from "@mui/material";
-import Visibility from "@mui/icons-material";
+import { DialogActions, InputLabel, TextField } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import MenuItem from "@mui/material";
 import "./user_register.css";
@@ -157,6 +158,44 @@ function UserRegister() {
             />
           ) : (
             <p className="required_txt">※必須</p>
+          )}
+        </div>
+        <div className="password_input">
+          <FormControl variant="outlined" className="password_txt">
+            <InputLabel htmlFor="outlined-pw">パスワード</InputLabel>
+            <OutlinedInput
+              id="outlined-pw"
+              label="パスワード"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              {...register("password", {
+                required: "パスワードを入力してください",
+                pattern: {
+                  value: /^(?=.*[A-Z]|[a-z])(?=.*\d)[A-Za-z0-9]{8,}$/, // 入力規則 8文字以上
+                  message: "パスワードの形式が違います",
+                },
+              })}
+            />
+          </FormControl>
+          {errorFlg ? (
+            <ErrorMessage
+              errors={errors}
+              name="password"
+              as="p"
+              className="error_message"
+            />
+          ) : (
+            <p className="required_txt">※必須（8文字以上 半角英数字）</p>
           )}
         </div>
       </form>
