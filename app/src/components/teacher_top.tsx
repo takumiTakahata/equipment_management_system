8d4b842a3b2ba888a761e53bbec973669c1e8ddf
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2024-08-11 10:26:11 +0900

    プルダウンの作成

--- app/src/components/teacher_top.tsx
+++ app/src/components/teacher_top.tsx
@@ -1,7 +1,19 @@
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+
 function TeacherTop() {
   return (
     <div>
       <h2>教員TOP</h2>
+      <FormControl sx={{ width: 100 }}>
+        <InputLabel id="input_label">貸出中</InputLabel>
+        <Select labelId="input_label">
+          <MenuItem value="">貸出中</MenuItem>
+          <MenuItem value="">貸出前</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 }
