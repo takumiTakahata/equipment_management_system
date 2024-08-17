56d68432108e805221d9e4b14de1870bd5772ec1
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-08-17 17:57:59 +0900

    リストの設定

--- app/src/components/product_list.tsx
+++ app/src/components/product_list.tsx
@@ -1,4 +1,28 @@
 import React from "react";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import { ListItem } from "@mui/material";
+import { ListItemButton } from "@mui/material";
+
+function renderRow(props: ListChildComponentProps) {
+  const { index, style } = props;
+
+  return (
+    <ListItem style={style} key={index} component="div" disablePadding>
+      <ListItemButton className="list_button">
+        <div className="contents">
+          <p className="product_name">備品名:</p>
+          <p className="name">
+            いちばんやさしいITパスポート絶対合格の教科書+出る順問題集
+          </p>
+          <p className="limit">貸出期限:3ヶ月</p>
+        </div>
+        <div className="green_circle">
+          <p>貸出可</p>
+        </div>
+      </ListItemButton>
+    </ListItem>
+  );
+}
 
 function ProductList() {
   return (
