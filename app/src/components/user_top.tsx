1ea6876596bccd8cb176d4efe8b0f3122c100ccf
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-08-20 17:16:46 +0900

    import文追加

--- app/src/components/user_top.tsx
+++ app/src/components/user_top.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+import { ListItem } from "@mui/material";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import ListItemButton from "@mui/material/ListItemButton";
 function UserTop() {
   return (
     <div>
