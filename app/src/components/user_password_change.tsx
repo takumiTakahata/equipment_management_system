ce4ec0ecb163da9fbe660b43c4e96aad042a1b28
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-08-20 11:20:38 +0900

    useState文追加

--- app/src/components/user_password_change.tsx
+++ app/src/components/user_password_change.tsx
@@ -11,7 +11,23 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import InputAdornment from "@mui/material/InputAdornment";
 import "./user_password_change.css";
 
+interface FormInputs {
+  password: string;
+  check_password: string;
+}
+
+const handleMouseDownPassword = (
+  event: React.MouseEvent<HTMLButtonElement>
+) => {
+  event.preventDefault();
+};
+
 function UserPasswordChange() {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [errorFlg, setErrorFlg] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [mail, setMail] = React.useState("");
+
   return (
     <div>
       <p>パスワード変更</p>
