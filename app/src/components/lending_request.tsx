d25e3de15d25926ce79eca6dd12df24a85cc6ec5
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-08-20 16:33:16 +0900

    import文追加

--- app/src/components/lending_request.tsx
+++ app/src/components/lending_request.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { ListItem } from "@mui/material";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import ListItemButton from "@mui/material/ListItemButton";
+import "./lending_request.css";
 
 function LendingRequest() {
   return (
