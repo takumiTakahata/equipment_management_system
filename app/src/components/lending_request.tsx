312b3b855b59c20ece9cb0709da2cdea18b5d383
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-08-20 16:33:51 +0900

    RenderRowの作成

--- app/src/components/lending_request.tsx
+++ app/src/components/lending_request.tsx
@@ -6,6 +6,24 @@ import Button from "@mui/material/Button";
 import ListItemButton from "@mui/material/ListItemButton";
 import "./lending_request.css";
 
+function renderRow(props: ListChildComponentProps) {
+  const { index, style } = props;
+
+  return (
+    <ListItem style={style} key={index} component="div" disablePadding>
+      <ListItemButton className="list_button">
+        <div className="contents">
+          <p className="product_name">備品名:</p>
+          <p className="name">
+            いちばんやさしいITパスポート絶対合格の教科書+出る順問題集
+          </p>
+          <p className="limit">返却期限:2024/08/20</p>
+        </div>
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
 function LendingRequest() {
   return (
     <div>
