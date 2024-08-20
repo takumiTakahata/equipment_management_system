4063fdb430e14ca10c841051c0dfad541f6349b7
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-08-20 12:19:31 +0900

    useState,エラー設定

--- app/src/components/user_password_reset.tsx
+++ app/src/components/user_password_reset.tsx
@@ -3,7 +3,31 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useForm } from "react-hook-form";
 
+interface FormInputs {
+  email: string;
+}
+
 function UserPasswordReset() {
+  const [errorFlg, setErrorFlg] = React.useState(false);
+  const [mail, setMail] = React.useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+  //エラーじゃないときにしか動作しない
+  const onSubmit = (data: FormInputs) => {
+    setMail(data.email);
+  };
+  const onError = (errors: Object) => {
+    console.log(errors);
+    if (Object.keys(errors).length > 0) {
+      setErrorFlg(true);
+    } else {
+      setErrorFlg(false);
+    }
+  };
   return (
     <div>
       <p>パスワード変更用メール</p>
