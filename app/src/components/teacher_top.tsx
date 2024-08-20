164a2ed0b436e94bff6768d3738128c127bfbc6c
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2024-08-20 12:13:34 +0900

    円を追加

--- app/src/components/teacher_top.tsx
+++ app/src/components/teacher_top.tsx
@@ -13,6 +13,7 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import "./teacher_top.css";
 
 function createLoanHistory(
   loan_date: string, //貸出日
