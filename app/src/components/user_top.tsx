f024ce8c4d95e1f416a8a54c9d5fd44127c03f98
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-08-20 17:17:25 +0900

    RenderRowの追加

--- app/src/components/user_top.tsx
+++ app/src/components/user_top.tsx
@@ -4,6 +4,25 @@ import { FixedSizeList, ListChildComponentProps } from "react-window";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import ListItemButton from "@mui/material/ListItemButton";
+
+function renderRow(props: ListChildComponentProps) {
+  const { index, style } = props;
+
+  return (
+    <ListItem style={style} key={index} component="div" disablePadding>
+      <ListItemButton className="list_button">
+        <div className="contents">
+          <p className="product_name">備品名:</p>
+          <p className="name">
+            いちばんやさしいITパスポート絶対合格の教科書+出る順問題集
+          </p>
+          <p className="limit">返却期限:2024/08/20</p>
+        </div>
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
 function UserTop() {
   return (
     <div>
