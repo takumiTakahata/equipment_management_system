9f37ae25298e6cce99a21f67f24f2e69839f882d
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-08-22 00:21:49 +0900

    シリアライザーの修正

--- equipment_management/equipment_management_api/serializers/equipment_serializer.py
+++ equipment_management/equipment_management_api/serializers/equipment_serializer.py
@@ -6,8 +6,11 @@ class EquipmentSerializer(serializers.ModelSerializer):
     categories_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source='categories')
     class Meta:
         model = Product
-        fields = ['id','name','ISBN','categories_id','deadline',]
+        fields = ['id','name','ISBN','categories_id','deadline','lost_status','active_flag','delete_flag']
         extra_kwargs = {
             'ISBN': {'required': False},
             'categories_id': {'required': False},
+            'lost_status': {'required': False},
+            'active_flag': {'required': False},
+            'delete_flag': {'required': False},
         }
