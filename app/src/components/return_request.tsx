e9362a170eb325712fac67f0e50f10eedd33fee5
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-08-21 09:35:29 +0900

    import文追加

--- app/src/components/return_request.tsx
+++ app/src/components/return_request.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { ListItem } from "@mui/material";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import ListItemButton from "@mui/material/ListItemButton";
 
 function ReturnRequest() {
   return (
