94481b55e9d6346787e48177d04b99308e433bb4
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2024-08-22 11:41:25 +0900

    Headerの挿入・buttonの挿入

--- app/src/components/inventory_history.tsx
+++ app/src/components/inventory_history.tsx
@@ -1,9 +1,12 @@
-import { Inventory } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import Header from "./header";
 
 function InventoryHistory() {
   return (
-    <div>
+    <div id="iventory_history">
+      <Header />
       <h2>棚卸履歴</h2>
+      <Button variant="outlined">棚卸</Button>
     </div>
   );
 }
