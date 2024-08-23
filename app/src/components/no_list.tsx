24cdd2e9fc41ff824c9c43085df8d03017d78fe6
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2024-08-23 10:20:11 +0900

    一覧の値の定義

--- app/src/components/no_list.tsx
+++ app/src/components/no_list.tsx
@@ -1,3 +1,21 @@
+function createNoList(id: number, name: string, deadline: string) {
+  return {
+    id, //備品id
+    name, //備品名
+    deadline, //返却期限
+  };
+}
+
+const data: [number, string, string][] = [
+  [1, "ITパスポート", "3ヶ月"],
+  [2, "基本情報", "1ヶ月"],
+  [3, "応用情報", "1ヶ月"],
+];
+
+const rows = data.map((item) => {
+  return createNoList(...item);
+});
+
 function NoList() {
   return (
     <div>
