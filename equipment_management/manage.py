81a4691c3012588444226cf67b2a78d2d6cf5fb2
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-08-24 15:50:33 +0900

    manage.pyを初期化

--- equipment_management/manage.py
+++ equipment_management/manage.py
@@ -1,20 +1,40 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+# #!/usr/bin/env python
+# """Django's command-line utility for administrative tasks."""
+# import os
+# import sys
+
+# def main():
+#     """Run administrative tasks."""
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'equipment_management.settings')
+
+#     # Check if 'runserver' is in sys.argv and set the port accordingly
+#     if 'runserver' in sys.argv:
+#         port_index = sys.argv.index('runserver') + 1
+#         if port_index < len(sys.argv):
+#             sys.argv[port_index] = f"0.0.0.0:{os.getenv('PORT', '8000')}"
+#         else:
+#             sys.argv.append(f"0.0.0.0:{os.getenv('PORT', '8000')}")
+
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
+
 import os
 import sys
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'equipment_management.settings')
-
-    # Check if 'runserver' is in sys.argv and set the port accordingly
-    if 'runserver' in sys.argv:
-        port_index = sys.argv.index('runserver') + 1
-        if port_index < len(sys.argv):
-            sys.argv[port_index] = f"0.0.0.0:{os.getenv('PORT', '8000')}"
-        else:
-            sys.argv.append(f"0.0.0.0:{os.getenv('PORT', '8000')}")
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -25,7 +45,5 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
     main()
-
