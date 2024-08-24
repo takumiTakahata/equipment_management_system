4776c9a2003048ce473e75988b59e7a6dd5d4dc3
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-08-24 20:19:08 +0900

    シークレットキーの修正

--- equipment_management/equipment_management/settings.py
+++ equipment_management/equipment_management/settings.py
@@ -25,8 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)t9d_&n41&^9#+r*r_7r(ip1*iqhfrf91hn!sv(s83-ek!s)47'
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("シークレットキーが設定されていません")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
