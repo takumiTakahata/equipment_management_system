f5ebbb353e5ddfe35d6eb6b13f010e6632ff5121
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-08-24 22:17:25 +0900

    ログインボタン

--- app/src/components/user_login.tsx
+++ app/src/components/user_login.tsx
@@ -118,6 +118,15 @@ const Login: React.FC = () => {
       {!passwordError && (
         <p className="ad_login_required_txt">※必須（8文字以上 半角英数字）</p>
       )}
+
+      <Button
+        onClick={handleLogin}
+        variant="outlined"
+        className="user_login_button"
+      >
+        ログイン
+      </Button>
+      {error && <p>{error}</p>}
     </div>
   );
 };
