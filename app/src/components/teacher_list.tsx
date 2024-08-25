0a87dfe247498cbcdfae15bbad52db03fe68322a
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-08-25 19:56:36 +0900

    教員一覧画面のエンドポイント修正

--- app/src/components/teacher_list.tsx
+++ app/src/components/teacher_list.tsx
@@ -22,7 +22,9 @@ function TeacherList() {
   useEffect(() => {
     const fetchTeachers = async () => {
       try {
-        const response = await fetch("http://127.0.0.1:8000/api/teacher/");
+        const response = await fetch(
+          "https://mysite-mczi.onrender.com/api/teacher/"
+        );
         if (!response.ok) {
           throw new Error("Failed to fetch teachers");
         }
