9436697ebb2958f52309e6405fcb921bbe9e2762
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-11-25 16:46:13 +0900

    google books apiからopenbdへのapiリクエスト変更

--- equipment_management/equipment_management_api/views/book.py
+++ equipment_management/equipment_management_api/views/book.py
@@ -7,26 +7,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class BookView(APIView):
-  def get(self, request, isbn):
-    if not isbn:
-        return JsonResponse({'error': 'ISBNが提供されていません'}, status=400)
+class BookAPIView:
+    def get(self, request, isbn):
+        if not isbn:
+            return JsonResponse({'error': 'ISBNが提供されていません'}, status=400)
 
-    api_key = os.getenv('BOOK_API_KEY')
-    url = f'https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={api_key}'
+        # OpenBDのAPIエンドポイント
+        url = f'https://api.openbd.jp/v1/get?isbn={isbn}'
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
-        if data['totalItems'] == 0:
-            return JsonResponse({'error': '本が見つかりませんでした'}, status=404)
+        try:
+            # OpenBD APIへのリクエスト
+            response = requests.get(url)
+            response.raise_for_status()
 
-        book_data = data['items'][0]['volumeInfo']
-        if 'title' in book_data:
-            return JsonResponse({'title': book_data['title']})
-        else:
-            return JsonResponse({'error': '本が見つかりませんでした'}, status=404)
-    except requests.RequestException as e:
-        print("APIリクエスト中にエラーが発生しました:", e)
-        return JsonResponse({'error': 'APIリクエスト中にエラーが発生しました'}, status=500)
+            # レスポンスを解析
+            data = response.json()
+
+            # データが存在しない場合
+            if not data or data[0] is None:
+                return JsonResponse({'error': '本が見つかりませんでした'}, status=404)
+
+            # 必要な情報を取得
+            book_data = data[0]['summary']
+            if 'title' in book_data:
+                return JsonResponse({'title': book_data['title']})
+            else:
+                return JsonResponse({'error': 'タイトル情報が見つかりませんでした'}, status=404)
+        except requests.RequestException as e:
+            print("APIリクエスト中にエラーが発生しました:", e)
+            return JsonResponse({'error': 'APIリクエスト中にエラーが発生しました'}, status=500)
